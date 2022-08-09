8fc36c4c5a26b1bef31984954a3f533fe03fc95e
Nethaniel Gabay <nethanielmaoz@gmail.com>
2022-08-09 18:20:50 +0300

    refactoring

--- whist-home-assignment/server/index.js
+++ whist-home-assignment/server/index.js
@@ -7,8 +7,6 @@ app.use(cors())
 app.get('/', async (req, res) => {
     res.send({
         publicIp: address.ip(),
-        url: req.url
     })
 })
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`app listening on http://localhost:${port}`));
+app.listen(9090, () => console.log(`app listening on ${address.ip()}`));
